73ff0607625ee35a70f66b385731e78678af83c5
Felix Bell <felix@andre-bell.de>
2022-01-05 18:23:44 +0100

    two Bottom Challenge alignment left and right

--- two Button Challenge/ViewController.swift
+++ two Button Challenge/ViewController.swift
@@ -9,20 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
-    var ersterklick: Bool = true
+    var ersterklickA: Bool = true
+    var ersterklickB: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("viewDidload has run!")
         messageLabel.text = "A or B"
-        ersterklick = true
+        ersterklickA = true
+        ersterklickB = true
     }
     
     @IBAction func A(_ sender: UIButton) {
-        messageLabel.text = "Awesome"
+        if ersterklickA {
+            messageLabel.text = "Awesome"
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = .right
+            ersterklickA = false
+        } else {
+            messageLabel.text = "Awesome"
+            messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = .left
+            ersterklickA = true
+        }
     }
+    
     @IBAction func B(_ sender: UIButton) {
-        messageLabel.text = "BOOM"
+        if ersterklickB {
+            messageLabel.text = "BOOM"
+            messageLabel.textColor = .red
+            messageLabel.textAlignment = .left
+            ersterklickB = false
+        } else {
+            messageLabel.text = "BOOM"
+            messageLabel.textColor = .red
+            messageLabel.textAlignment = .right
+            ersterklickB = true
+        }
     }
     
 }
