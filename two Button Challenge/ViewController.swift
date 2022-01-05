cc90bda88e26c993134da74e0ba86a256391a8a6
Felix Bell <felix@andre-bell.de>
2022-01-05 17:40:48 +0100

    Two Bottom Challenge completed

--- two Button Challenge/ViewController.swift
+++ two Button Challenge/ViewController.swift
@@ -8,13 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    var ersterklick: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print ("viewDidload has run!")
+        messageLabel.text = "A or B"
+        ersterklick = true
     }
-
-
+    
+    @IBAction func A(_ sender: UIButton) {
+        messageLabel.text = "Awesome"
+    }
+    @IBAction func B(_ sender: UIButton) {
+        messageLabel.text = "BOOM"
+    }
+    
 }
 
